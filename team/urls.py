8049ef76348e2a52ae70536345e5b7c7e4d0dffb
Nick Winans <nw5zp@virginia.edu>
2020-06-24 16:14:07 -0400

    added team view

--- team/urls.py
+++ team/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "team"
 
 urlpatterns = [
+    path('', views.view, name='view'),
 ]
--- team/views.py
+++ team/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view(request):
+    return render(template_name='team/view.html', request=request)
