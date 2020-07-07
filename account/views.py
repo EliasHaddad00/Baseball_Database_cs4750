b7af93f7e2981c121442ea410e7ac592c00cd899
Nick Winans <nw5zp@virginia.edu>
2020-07-07 16:13:58 -0400

    update profile edit logic

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from django.db.utils import IntegrityError
 
 @login_required
 def view(request):
@@ -26,14 +27,17 @@ def edit(request):
         u.first_name = request.POST.get('first_name')
         u.last_name = request.POST.get('last_name')
 
-        if request.POST.get('email') is not None:
+        if request.POST.get('email') != "" and request.POST.get('email') != request.user.email:
             u.email = request.POST.get('email')
             u.username = request.POST.get('email')
 
-        if request.POST.get('password') is not None:
+        if request.POST.get('password') != "":
             u.set_password(request.POST.get('password'))
 
-        u.save()
+        try:
+            u.save()
+        except IntegrityError:
+            return HttpResponse("This email address is already in use, please use a different one")
         return redirect(reverse('profile:view'))
     elif request.method == 'GET':
         return render(template_name='account/edit.html', request=request)
@@ -41,6 +45,9 @@ def edit(request):
 def login(request):
     if request.method == "POST":
         u = authenticate(username=request.POST.get('email'), password=request.POST.get('password'))
+        print(u)
+        if u is None:
+            return render(template_name='account/login.html', request=request)
         auth_login(request, u)
         return redirect(reverse('profile:view'))
     elif request.method == 'GET':
