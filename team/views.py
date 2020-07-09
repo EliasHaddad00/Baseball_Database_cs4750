23b9ea488675ca08e19c4fbc535c9e1e35035a94
Josh Fitzpatrick <jtfitzpa@umich.edu>
2020-07-09 16:50:55 -0400

    added in email for manager and edited views.py to add in coaches and managers for teams

--- team/views.py
+++ team/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
-from sluggers.models import Team, Match
+from sluggers.models import Team, Match, Coach, Manager
 
 from datetime import datetime
 
@@ -44,5 +44,31 @@ def edit(request, team_id):
         if request.POST.get('team_name') != "":
             team.team_name = request.POST.get('team_name')
             team.save()
+
+        c_first_name = request.POST.get('coach_first_name')
+        c_last_name = request.POST.get('coach_last_name')
+        c_phone_number = request.POST.get('coach_phone_number')
+        c_email = request.POST.get('coach_email')
+
+        m_first_name = request.POST.get('manager_first_name')
+        m_last_name = request.POST.get('manager_last_name')
+        m_phone_number = request.POST.get('manager_phone_number')
+        m_email = request.POST.get('manager_email')
+
+        coach_info = Coach.objects.get(coach_id=team.coach.coach_id)
+        manager_info = Manager.objects.get(manager_id=team.manager.manager_id)
+
+        
+        coach_info.coach_first = c_first_name
+        coach_info.coach_last = c_last_name
+        coach_info.coach_phone = c_phone_number
+        coach_info.coach_email = c_email
+        coach_info.save()
+
+        manager_info.coach_first = m_first_name
+        manager_info.coach_last = m_last_name
+        manager_info.coach_phone = m_phone_number
+        manager_info.coach_email = m_email
+        manager_info.save()
 
         return redirect(reverse('team:view', args=[team_id]))
